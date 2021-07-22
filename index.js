3beb26363e7951a3977d2fe5c43f44ad7ce7b002
prabhuomk <prabhuomk@gmail.com>
2021-07-23 03:01:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+import express from "express";
+import {MongoClient} from "mongodb";
+
+const app = express();
+const PORT=5000;
+
+
+   async function createConnection (){
+       const MONGO_URL="mongodb+srv://prabhu:prabhu007@cluster0.cqxro.mongodb.net/contestants?retryWrites=true&w=majority";
+       const client = new MongoClient(MONGO_URL);
+
+       try{
+           await client.connect();
+           console.log("successfully connected");
+           return client;
+          
+       }
+       catch(err) {
+           console.log(err);
+
+       }
+   }
+
+   async function getPolls(client,filter)
+   {
+    const result=await client.db("contestant").collection("poll").find(filter).toArray();
+    console.log("successfully found all contentant",result);
+    return result;
+   }
+   
+ 
+
+   async function getPollById(client,id)
+   {
+    const result=await client.db("contestant").collection("poll").findOne({id:id});
+    console.log("successfully found poll by id",result);
+    return result;
+   }
+
+   app.get("/",(request,response)=>{
+    response.send("welcome to app");
+});
+   
+ app.get("/poll", async (request,response)=>{
+    const client=await createConnection();
+    const contestants=await getPolls(client,{});
+    response.send(contestants);
+ });
+
+app.get("/poll/content/:search", async (request,response)=>{
+    const search = request.params.search;
+    const client=await createConnection();
+    const contestants=await getPolls(client,{content:{$regex:search , $options:"i"}});
+    response.send(contestants);
+ });
+
+
+ app.get("/poll/name/:companyname", async (request,response)=>{
+    const companyname = request.params.companyname;
+    const client=await createConnection();
+    const contestants=await getPolls(client,{company:companyname});
+    response.send(contestants);
+ });
+
+app.get("/poll/:id", async (request,response)=>{
+    const id = request.params.id;
+    const client=await createConnection();
+    const contestant=await getPollById(client,id);
+    response.send(contestant);
+    
+});
+
+app.listen(PORT,()=>console.log("the server started",PORT));
